5dbd0b700b49e2dcac3c90c956fe1afa90c597bd
Max Cortes <maxcortes23@hotmail.com>
2025-08-01 11:38:37 -0600

    Add support for multiple games

--- Web_scraping_Steam.py
+++ Web_scraping_Steam.py
@@ -1,3 +1,4 @@
+import json
 import requests
 from bs4 import BeautifulSoup
 import smtplib
@@ -12,15 +13,14 @@ target_price = 40
 sender_email = os.environ["EMAIL_USER"]
 sender_pass = os.environ["EMAIL_PASS"]
 receiver_email = os.environ["EMAIL_USER"]
-game_agecheck_url = f"https://store.steampowered.com/agecheck/app/{game_id}/"
-game_url = f"https://store.steampowered.com/app/{game_id}/"
 
-def send_email_notification(sender_email: str, receiver_email: str, discount=False):
-    subject_discount = f"Steam discount!!!!!, {game_name}, actual price: {price_value}"
-    body_discount = f"There is a discount for {game_name}, actual price: {price_value}"
 
-    subject_no_discount = f"No discount for {game_name}, actual price: {price_value}, expected: {target_price} "
-    body_no_discount = f"No discount for {game_name}, actual price: {price_value}, expected: {target_price}"
+def send_email_notification(sender_email: str, receiver_email: str, game_name: str, target_price: float, actual_price: float, discount=False):
+    subject_discount = f"Steam discount!!!!!, {game_name}, actual price: {actual_price}"
+    body_discount = f"There is a discount for {game_name}, actual price: {actual_price}"
+
+    subject_no_discount = f"No discount for {game_name}, actual price: {actual_price}, expected: {target_price} "
+    body_no_discount = f"No discount for {game_name}, actual price: {actual_price}, expected: {target_price}"
 
     msg = MIMEText(body_discount if discount else body_no_discount)
     msg["Subject"] = subject_discount if discount else subject_no_discount
@@ -31,40 +31,72 @@ def send_email_notification(sender_email: str, receiver_email: str, discount=Fal
         server.login(sender_email, sender_pass)
         server.send_message(msg)
 
-session = requests.Session()
+import re
 
-session.headers.update({
-   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/115.0.0.0 Safari/537.36" 
-})
+def parse_price(price_text: str) -> float:
+    if "₡" in price_text:        
+        price_clean = re.sub(r"[^\d]", "", price_text) 
+        return float(price_clean)
+    elif "$" in price_text:        
+        price_clean = re.sub(r"[^\d.]", "", price_text) 
+        return float(price_clean)
+    else:        
+        price_clean = re.sub(r"[^\d.]", "", price_text)
+        try:
+            return float(price_clean)
+        except ValueError:
+            return -1
 
-session.post(game_agecheck_url, data={
-    "ageDay": "1",
-    "ageMonth": "January",
-    "ageYear": "1990"
-})
 
-session.cookies.set("birthtime", "568022401", domain=".steampowered.com")
-session.cookies.set("lastagecheckage", "1-January-1990", domain=".steampowered.com")
+def get_game_price(game):
+    game_id = game["id"]
+    game_name = game["name"]
+    game_target_price = game["target_price"]
+    game_agecheck_url = f"https://store.steampowered.com/agecheck/app/{game_id}/?cc=us"
+    game_url = f"https://store.steampowered.com/app/{game_id}/?cc=us"
+    
 
-response = session.get(game_url)
+    session = requests.Session()
+    session.headers.update({
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/115.0.0.0 Safari/537.36" 
+    })
 
-if response.status_code == 200:
+    session.post(game_agecheck_url, data={
+        "ageDay": "1",
+        "ageMonth": "January",
+        "ageYear": "1990"
+    })
+
+    session.cookies.set("Steam_Language", "english", domain=".steampowered.com")
+    session.cookies.set("birthtime", "568022401", domain=".steampowered.com")
+    session.cookies.set("lastagecheckage", "1-January-1990", domain=".steampowered.com")
+    session.cookies.set("wants_currency", "1", domain=".steampowered.com")
+
+    response = session.get(game_url)
+
+    if response.status_code != 200:
+        print(f"Status code: {response.status_code} ")
+        return None
+    
     soup = BeautifulSoup(response.text, "html.parser")
-    game_price = soup.find("div", class_="game_purchase_price price")
-    if not game_price:
-        game_price = soup.find("div", class_="discount_final_price")
+    game_price = soup.find("div", class_="game_purchase_price price") or soup.find("div", class_="discount_final_price")
+    print(f"[DEBUG] {game_name} - raw price tag: '{game_price}'")
 
     if game_price:
-        game_price_str = game_price.get_text(strip=True)
-        price_clean = re.sub(r"[^\d]", "", game_price_str)
-        price_value = int(price_clean) / 100
-        print(f"{game_name} price is: {price_value}")
+        price_text = game_price.get_text(strip=True)
+        price_value = parse_price(price_text)
 
-        if price_value <= target_price:            
-            send_email_notification(sender_email=sender_email, receiver_email=receiver_email,discount=True)
-        else:
-            send_email_notification(sender_email=sender_email, receiver_email=receiver_email)
-    else:        
-        print("Game price not found.")
-else:
-    print(f"Execution error, response code: {response.status_code}")
+    
+    if price_value <= target_price:                 
+        send_email_notification(sender_email=sender_email, receiver_email=receiver_email, game_name=game_name, target_price=game_target_price, actual_price=price_value, discount=True)
+    else:
+        send_email_notification(sender_email=sender_email, receiver_email=receiver_email, game_name=game_name, target_price=game_target_price, actual_price=price_value, discount=False)
+
+   
+with open("games.json", "r") as file:
+    games = json.load(file)
+
+for game in games:
+    get_game_price(game)
+        
+
